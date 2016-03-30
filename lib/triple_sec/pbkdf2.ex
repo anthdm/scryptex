8caeb7c07775c027206db849dc6b4fae48c04301
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2016-03-30 21:35:24 +0200

    Clean up pbkdf2

--- lib/triple_sec/pbkdf2.ex
+++ lib/triple_sec/pbkdf2.ex
@@ -4,39 +4,35 @@ defmodule TripleSec.Pbkdf2 do
   @max_length (1 <<< 32) - 1
   @salt_range 16..1024
 
-  def pbkdf2(_password, _salt, iterations, _length, _prf)
+  def pbkdf2(_pass, _salt, iterations, _length, _prf)
     when iterations <= 0,
     do: raise ArgumentError, message: "iterations has to be positive"
 
-  def pbkdf2(_password, _salt, _iterations, length, _prf)
+  def pbkdf2(_pass, _salt, _iterations, length, _prf)
     when length >= @max_length,
     do: raise ArgumentError, message: "length should be less than #{@max_length}"
 
-  def pbkdf2(_password, salt, _iterations, _length, _prf)
-    when not byte_size(salt) in @salt_range,
-    do: raise ArgumentError, message: "salt size should be within #{inspect @salt_range}"
+  # def pbkdf2(_pass, salt, _iterations, _length, _prf)
+  #   when not byte_size(salt) in @salt_range,
+  #   do: raise ArgumentError, message: "salt size should be within #{inspect @salt_range}"
 
-  def pbkdf2(password, salt, iterations, length, prf),
-    do: pbkdf2(password, salt, iterations, length, prf, 1, [], 0)
+  def pbkdf2(pass, salt, iterations, length, prf),
+    do: pbkdf2(pass, salt, iterations, length, prf, 1, [], 0)
 
-  defp pbkdf2(password, salt, iterations, max_length, prf, block_ix, acc, length)
+  defp pbkdf2(pass, salt, iterations, max_length, prf, block_ix, acc, length)
       when length < max_length do
-    pseudo = prf.(password, <<salt::binary, block_ix::32>>)
-    block = iterate(password, prf, iterations-1, pseudo, pseudo)
-    pbkdf2(password, salt, iterations, max_length, prf, block_ix+1, [acc|block], length+byte_size(block))
+    pseudo = prf.(pass, <<salt::binary, block_ix::32>>)
+    block = iterate(pass, prf, iterations-1, pseudo, pseudo)
+    pbkdf2(pass, salt, iterations, max_length, prf, block_ix+1, [acc|block], length+byte_size(block))
   end
-  defp pbkdf2(_password, _salt, _iterations, max_length, _prf, _block_ix, acc, _length) do
+  defp pbkdf2(_pass, _salt, _iterations, max_length, _prf, _block_ix, acc, _length) do
     <<result::binary-size(max_length), _::binary>> = IO.iodata_to_binary(acc)
     result
   end
 
-  defp iterate(_password, _prf, 0, _prev, acc), do: acc
-  defp iterate(password, prf, iteration, prev, acc) do
-    next = prf.(password, prev)
-    iterate(password, prf, iteration-1, next, :crypto.exor(next, acc))
-  end
-
-  def hmac_sha512 do
-    &:crypto.hmac(:sha512, &1, &2)
+  defp iterate(_pass, _prf, 0, _prev, acc), do: acc
+  defp iterate(pass, prf, iteration, prev, acc) do
+    next = prf.(pass, prev)
+    iterate(pass, prf, iteration-1, next, :crypto.exor(next, acc))
   end
 end
